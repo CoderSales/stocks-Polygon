995fb1851e4c8fc36fb9ebb815b04874dbb0b39e
Coder731 <stephenobrien1@gmail.com>
2023-08-14 14:34:35 +0100

    Update stock js
    To take
    user input
    from CLI
    and
    store it
    to variables

--- stock.js
+++ stock.js
@@ -1,8 +1,23 @@
 // Declare variables using const and let (not var)
-let url = ""
-let stock = ""
-let timeStart = ""
-let timeEnd = ""
+// let url = ""
+// let stock = ""
+// let timeStart = ""
+// let timeEnd = ""
+
+// Declare variables by taking user input from CLI
+
+// Reference: https://linuxhint.com/get-user-input-from-command-line-with-javascript/#:~:text=Conclusion-,To%20get%20the%20user%20input%20from%20the%20command%20line%20with,prompts%20the%20user%20for%20input.
+
+// take url from User in CLI
+let url = prompt("url:")
+// take stock ticker from User in CLI
+let stock = prompt("stock ticker:")
+// take start time from User in CLI
+let timeStart = prompt("start of date range:")
+// take end time from User in CLI
+let timeEnd = prompt("end of date range:")
+
+
 
 // We are using the Polygon.io API
 
